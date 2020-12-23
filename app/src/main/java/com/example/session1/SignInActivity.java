36b38e171ac6b3a1d649737d90cb76e0b7ea2b45
Maxim <devistyn.max1990@gmail.com>
2020-12-23 12:25:58 +0300

    first commit

---
+++ app/src/main/java/com/example/session1/SignInActivity.java
@@ -0,0 +1,31 @@
+package com.example.session1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity { // Класс SignInActivity используется для входа пользователя в приложение
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Session1);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+    public void onCreateAccount(View view) {
+        startActivity(new Intent(this, SignUpActivity.class)); // Метод переводит пользоаптеля на экран регистрации
+    }
+
+    public void onSignUp(View view) {
+    }
+
+    public void onForgotPassword(View view) {
+    }
+
+    public void onSignIn(View view) {
+    }
+
+}
